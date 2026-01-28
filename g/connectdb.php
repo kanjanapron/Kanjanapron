f0367af5fa39dc2156a0271a658d910cefe91c3e
Kanjanapron <kanjanapron2159@gmail.com>
2026-01-28 16:06:16 +0700

    a

--- g/connectdb.php
+++ g/connectdb.php
@@ -1,7 +1,7 @@
 <?php
 		$host = "localhost";
 		$user = "root";
-		$pwd = "Golg@2004";
+		$pwd = "Golf@2004";
 		$db = "4122db";
 		$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
 		mysqli_query($conn, "SET NAMES utf8");
