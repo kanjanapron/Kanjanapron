78d01e3e7540c8ada7b14c3a23956faa1b6b05c6
kanjanapron <kanjanapron2159@gmail.com>
2026-02-04 15:19:38 +0700

    a

---
+++ hh/b.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>กาญจนาภรณ์ วินทะไชย (แตงกวา)</title>
+</head>
+
+<body>
+<h1>กาญจนาภรณ์ วินทะไชย (แตงกวา)</h1>
+
+<?php
+	echo @$_SESSION['name'] . "<br>";
+    echo @$_SESSION['nickname'] . "<br>";
+    echo @$_SESSION['nickname'] . "<br>";
+    echo @$_SESSION['nickname'] . "<br>";
+?>
+</body>
+</html>
