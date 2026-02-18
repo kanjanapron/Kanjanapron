71b9e97208d00ec2a4bbbc00a58d7dbb44de103f
Kanjanapron <kanjanapron2159@gmail.com>
2026-02-18 13:40:28 +0700

    1

--- j/a.php
+++ j/a.php
@@ -5,9 +5,10 @@
 <title>งาน j -- กาญจนาภรณ์ วินทะไชย (แตงกวา)</title>
 </head>
 <body>
+
 <h1>งาน j -- กาญจนาภรณ์ วินทะไชย (แตงกวา)</h1>
 
-<form method="post" action="" enctype="multipart/form-data">
+<form method="post" enctype="multipart/form-data">
     รหัสนิสิต <input type="text" name="std_id" required>
     ชื่อ <input type="text" name="std_name" required>
     รูปภาพ <input type="file" name="std_img" accept="image/*" required>
@@ -19,34 +20,43 @@
 <?php
 include_once("connectdb.php");
 
+// ================== INSERT ==================
 if(isset($_POST['Submit'])){
+
     $std_id = $_POST['std_id'];
     $std_name = $_POST['std_name'];
 
-    // จัดการอัปโหลดรูป
     $img_name = $_FILES['std_img']['name'];
     $tmp_name = $_FILES['std_img']['tmp_name'];
-    $folder = "img/".$img_name;
 
-    move_uploaded_file($tmp_name, $folder);
+    // ถ้ามีรูป
+    if($img_name != ""){
+        move_uploaded_file($tmp_name, "img/".$img_name);
+    }
 
-    $sql = "INSERT INTO students (s_id, std_id, std_name, std_img) 
-            VALUES (NULL, '$std_id', '$std_name', '$img_name')";
-    mysqli_query($conn, $sql);
+    $sql = "INSERT INTO students (std_id, std_name, std_img) 
+            VALUES ('$std_id', '$std_name', '$img_name')";
+
+    if(mysqli_query($conn, $sql)){
+        echo "<script>alert('บันทึกสำเร็จ');</script>";
+        echo "<meta http-equiv='refresh' content='0'>";
+    }else{
+        echo "Error: " . mysqli_error($conn);
+    }
 }
 ?>
 
 <table border="1">
-    <tr>
-        <th>ลำดับ</th>
-        <th>รหัสนิสิต</th>
-        <th>ชื่อ</th>
-        <th>รูปภาพ</th>
-        <th>ลบ</th>
-    </tr>
+<tr>
+    <th>ลำดับ</th>
+    <th>รหัสนิสิต</th>
+    <th>ชื่อ</th>
+    <th>รูปภาพ</th>
+    <th>ลบ</th>
+</tr>
 
 <?php
-$sql = "SELECT * FROM students";
+$sql = "SELECT * FROM students ORDER BY s_id DESC";
 $rs = mysqli_query($conn,$sql);
 
 while ($data = mysqli_fetch_array($rs)){
@@ -58,19 +68,18 @@ while ($data = mysqli_fetch_array($rs)){
     <td>
         <img src="img/<?php echo $data['std_img']; ?>" width="80">
     </td>
-    <td width="80" align="center">
-        <a href="delete_student.php?id=<?php echo $data['s_id']; ?>"
+    <td align="center">
+        <a href="delete_student.php?id=<?php echo $data['s_id']; ?>" 
            onclick="return confirm('ยืนยันการลบข้อมูลไหม?');">
-            <img src="img/1.png" width="20">
+            ❌
         </a>
     </td>
 </tr>
 <?php } ?>
 
 </table>
+
 </body>
 </html>
 
-<?php
-mysqli_close($conn);
-?>
+<?php mysqli_close($conn); ?>
